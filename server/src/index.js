58055a7fb76e6d75db11d00671f2dfd4c7196699
Danny (vai) San <vai.quoc.san@gmail.com>
2020-10-23 16:32:24 -0700

    created signup & sign in routes. implemented password encryption with bcrypt and authentication with jwt

--- server/src/index.js
+++ server/src/index.js
@@ -1,27 +1,34 @@
-require('dotenv').config()
+require('dotenv').config();
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 const app = express();
 const port = 3000;
 
-const mongoUri = `mongodb+srv://admin:${process.env.DB_PASS}@pathfinder.94gaj.mongodb.net/<dbname>?retryWrites=true&w=majority`
+app.use(bodyParser.json());
+app.use(authRoutes);
+
+const mongoUri = `mongodb+srv://admin:${process.env.DB_PASS}@pathfinder.94gaj.mongodb.net/<dbname>?retryWrites=true&w=majority`;
 
 mongoose.connect(mongoUri, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
-})
+});
 mongoose.connection.on('connected', () => {
-  console.log('Connected to mongoDB')
-})
+  console.log('Connected to mongoDB');
+});
 mongoose.connection.on('error', (err) => {
-  console.log('Error connecting to DB', err)
-})
+  console.log('Error connecting to DB', err);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hi')
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Youre email: ${req.user.email}`);
 });
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
---
+++ server/src/models/User.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+//! pre-save hook. Function runs before we save an instance of the user into DB
+//! used function keyword to assign context of 'this to user and prevent context of 'this' to this file
+userSchema.pre('save', function (next) {
+  const user = this;
+  //! Salt & Hash users password on sign up
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+//! method that gets attached to every user created to compare user's input pw with database pw
+userSchema.methods.comparePassword = function (candidatePassword) {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!isMatch) {
+        return reject(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
+
+mongoose.model('User', userSchema);
