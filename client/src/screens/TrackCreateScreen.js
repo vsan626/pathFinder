34daf61f6ffa189ef8ef28b91848c77ef99633cb
Danny (vai) San <vai.quoc.san@gmail.com>
2020-10-26 17:08:56 -0700

    boilerplate for navigation screens created and assigned different navigation flows with nested navigations

---
+++ client/src/screens/TrackCreateScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const TrackScreateScreen = () => {
+  return <Text style={{fontSize: 48}}>TrackScreateScreen</Text>
+}
+
+const styles = StyleSheet.create({});
+
+export default TrackScreateScreen;
